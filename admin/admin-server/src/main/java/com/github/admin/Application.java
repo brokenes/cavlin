4aa56a7c68c69fd7a7a35b61e7b1fb2711eb85da
谢用培 <xieyongpei@xieyongpeideMacBook-Pro.local>
2020-03-27 22:30:58 +0800

    添加启动打印日志信息

--- admin/admin-server/src/main/java/com/github/admin/Application.java
+++ admin/admin-server/src/main/java/com/github/admin/Application.java
@@ -16,5 +16,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class Application {
     public static void main( String[] args ){
         SpringApplication.run(Application.class, args);
+        System.err.println(">>>>>>>>>>>>>>>>>>admin-server已经启动<<<<<<<<<<<<<<<<<<<<<<");
     }
 }
--- pay-eureka/pay-eureka/src/main/java/com/github/pay/Application.java
+++ pay-eureka/pay-eureka/src/main/java/com/github/pay/Application.java
@@ -15,7 +15,7 @@ public class Application {
 	
     public static void main( String[] args ){
         SpringApplication.run(Application.class, args);
-        
+        System.err.println(">>>>>>>>>>>>>>>>>>enureka已经启动<<<<<<<<<<<<<<<<<<<<<<");
     }
     
 }
